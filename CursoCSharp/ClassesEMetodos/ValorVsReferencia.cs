76c21c515f5beee9da782ff44555d1e6e28e9786
EvertonDSS <everton.097@gmail.com>
2022-03-08 14:23:15 -0300

    Valor vs Referencia

--- CursoCSharp/ClassesEMetodos/ValorVsReferencia.cs
+++ CursoCSharp/ClassesEMetodos/ValorVsReferencia.cs
@@ -19,6 +19,21 @@ namespace CursoCSharp.ClassesEMetodos {
 
             Console.WriteLine($"{numero} {copiaNumero}");
 
+            Dependente dep = new Dependente() {
+                Nome = "Beto",
+                Idade = 20
+            };
+            Dependente copiaDep = dep;
+            
+            Console.WriteLine($"{dep.Nome} {copiaDep.Nome}");
+            Console.WriteLine($"{dep.Idade} {copiaDep.Idade}");
+
+            copiaDep.Nome = "Renato";
+            dep.Idade = 21;
+
+            Console.WriteLine($"{dep.Nome} {copiaDep.Nome}");
+            Console.WriteLine($"{dep.Idade} {copiaDep.Idade}");
+
         }
     }
 }
